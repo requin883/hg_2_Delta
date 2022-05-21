0689b43d05c536bd97d8b2518256d35355ce699b
josegalvis27 <jose.galvis.rivas@gmail.com>
2022-05-21 17:21:51 -0400

    feat: add /bravo

---
+++ bravo/hack2.js
@@ -0,0 +1,6 @@
+   //eliminar el primer item
+  let a = [100,200,300,400,500,600,700] 
+
+a.shift() // elimina el primer valor y devuelve dicho valor
+
+console.log(a)
---
+++ bravo/hack7.js
@@ -0,0 +1,6 @@
+//remplazar el item 300 por tú alias
+let a = [100,200,300,400,500,600,700]
+
+a.splice(2,1,'Bravo')
+
+console.log(a)
